94736f0f706eaaf3adc5990613164abe55918ed8
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-04-03 20:23:28 +0300

    Add practice event list view

--- Controllers/PracticeController.cs
+++ Controllers/PracticeController.cs
@@ -1,18 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Spinfluence.Services;
+#pragma warning disable
 
 namespace Spinfluence.Controllers
 {
     public class PracticeController : Controller
     {
+        readonly IPracticeService practiceService;
+
+        public PracticeController(IPracticeService practiceService)
+        { this.practiceService = practiceService; }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        /*[HttpPost]
-        public IActionResult AddPractice()
+        [HttpDelete]
+        public async Task<IActionResult> RemovePractice(int? id)
         {
-            return View();
-        }*/
+            string? token = Request.Cookies["token"];
+
+            if(id != null)
+            {
+                int res = await practiceService.CancelPracticeEventAsync(id.Value, token);
+                if (res == 1) return RedirectToAction("/Index");
+                else if (res < 0) return Redirect("/Account/Login");
+            }
+
+            return RedirectToAction("/Index");
+        }
     }
 }
