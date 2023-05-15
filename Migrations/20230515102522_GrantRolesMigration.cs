9eb1433e5c9e2622cc423f86a1f987a2bd0902ee
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-05-15 13:26:06 +0300

    Add account grant roles migration

---
+++ Migrations/20230515102522_GrantRolesMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spinfluence.Migrations
+{
+    public partial class GrantRolesMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "admin",
+                table: "Account");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GrantType",
+                table: "Account",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GrantType",
+                table: "Account");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "admin",
+                table: "Account",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
