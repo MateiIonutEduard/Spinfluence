6f7e5154b16ac8df5c416fb33e3813f068ab77a6
Matei Ionut-Eduard <ionuteduardm@gmail.com>
2023-05-15 12:02:38 +0300

    Fix entity framework database migrations

--- Data/SpinContextFactory.cs
+++ Data/SpinContextFactory.cs
@@ -5,6 +5,7 @@ namespace Spinfluence.Data
 {
     public class SpinContextFactory : IDesignTimeDbContextFactory<SpinContext>
     {
+        /* This fix is needed to Entity Framework to create database migrations successful. */
         public SpinContext CreateDbContext(string[] args)
         {
             IConfigurationRoot configurationRoot = new ConfigurationBuilder()
